98dd90bb39bcaa2287bf089350da2c5401512f12
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-04 14:39:43 +0000

    Created Profile model and added static files for the project

--- fitnessblog/fitnessblog/settings.py
+++ fitnessblog/fitnessblog/settings.py
@@ -118,8 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-yoesk8-project4fitnessb-q8lo9rx4k1t.ws-eu80.gitpod.io']
