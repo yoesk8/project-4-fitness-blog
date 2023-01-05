7230761b3a47b39d279c853c140ea4fb221c4f4b
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-01-05 04:54:50 +0000

    Added follow and unfollow functionality

--- fitnessblog/core/urls.py
+++ fitnessblog/core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
     path('like-post', views.like_post, name='like-post'),
     path('profile/<str:pk>', views.profile, name='profile'),
 ]
